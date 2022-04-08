521852f5570fb4842b1c93756aee14ee9074cab5
Mihaly Papay <mihaly.papay@webpont.com>
2022-04-08 15:32:14 +0200

    update hangman module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HangmanSvgComponent } from './components/hangman-svg/hangman-svg.component';
 import { HangmanTextComponent } from './components/hangman-text/hangman-text.component';
 import { HangmanKeyboardComponent } from './components/hangman-keyboard/hangman-keyboard.component';
 import { HangmanComponent } from './components/hangman/hangman.component';
+import { HttpClientModule  } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,14 @@ import { HangmanComponent } from './components/hangman/hangman.component';
     HangmanSvgComponent,
     HangmanTextComponent,
     HangmanKeyboardComponent,
-    HangmanComponent
+    HangmanComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule ,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hangman/hangman.component.ts
+++ src/app/components/hangman/hangman.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HangmanService } from './../../service/hangman.service';
 
 @Component({
   selector: 'app-hangman',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HangmanComponent implements OnInit {
 
-  constructor() { }
+  public jsonPath = 'assets/words.json';
+  public questions: string[] = [];
+  public questionLength: number = 0;
+  public questionLengths: number[] = [];
+
+  constructor(private HangmanService: HangmanService) { }
 
   ngOnInit(): void {
+    this.HangmanService.getQuestions(this.jsonPath).subscribe((response) => {
+      this.questions = response.items;
+      this.getQuestionLengths();
+      console.log(this.questions);
+    })
   }
 
+  public getQuestionLengths(): void{
+    this.questions.forEach(question => {
+      if(!this.questionLengths.includes(question.length)){
+        this.questionLengths.push(question.length)
+      }
+    });
+    console.log(this.questionLengths)
+  }
+
+  public getQuestionWithLength(event: any){
+    console.log(event.questionLength);
+    this.questionLength = event.questionLength;
+  }
 }
