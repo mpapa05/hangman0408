05c70c0ed4e9d7dd460aba7e351d19c81ef0f27d
Mihaly Papay <mihaly.papay@webpont.com>
2022-04-08 14:35:25 +0200

    update index, styles, hangman-comp, words

--- src/app/service/hangman.service.ts
+++ src/app/service/hangman.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const defaultJSONPath = 'assets/words.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HangmanService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getQuestions(jsonPath: string = defaultJSONPath) {
+    return this.http.get<{ items: string[] }>(jsonPath);
+  }
 }
